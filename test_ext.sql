6a43fde7209a1c71ae462646f96c812a4950658c
HHT <harashtatandjung@gmail.com>
2020-03-03 18:49:20 +0700

    test2

--- test_ext.sql
+++ test_ext.sql
@@ -4,3 +4,4 @@ GO;
 
 -- #https://docs.microsoft.com/en-us/sql/visual-studio-code/sql-server-develop-use-vscode?view=sql-server-ver15
 -- https://www.youtube.com/watch?v=7_fNMy6cKEk
+-- https://dzone.com/articles/sql-with-visual-studio-code
