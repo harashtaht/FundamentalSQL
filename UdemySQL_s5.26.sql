1d8cfa3de5d490d639c0c7a9dbc58269b3637cdc
HHT <harashtatandjung@gmail.com>
2020-03-03 20:12:28 +0700

    L45

--- UdemySQL_s5.26.sql
+++ UdemySQL_s5.26.sql
@@ -32,3 +32,38 @@ USE Sales;
 
 -- show tables;
 
+CREATE TABLE sales
+(
+	purchase_number INT NOT NULL PRIMARY KEY,
+	date_of_purchase DATE NOT NULL,
+    customer_id INT,
+    item_code VARCHAR(10) NOT NULL
+    );
+
+
+-- Create the “customers” table in the “sales” database. 
+-- Let it contain the following 5 columns: 
+-- customer_id, first_name, last_name, email_address, and number_of_complaints. 
+-- Let the data types of customer_id and number_of_complaints be integer, 
+-- while the data types of all other columns be VARCHAR of 255.
+
+CREATE TABLE customers
+(
+	customer_id INT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email_address VARCHAR(255) NOT NULL,
+    number_of_complaints INT
+    );
+    
+-- What's inside the tables (the properties) is called "Field"
+
+show tables;
+select * from sales;
+select * from customers;
+
+SELECT * FROM sales.customers;
+
+select * from sales.sales;
+
+DROP TABLE SALES;
