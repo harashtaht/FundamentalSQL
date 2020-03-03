c4c282c3af76904d0efcaec38e91894a9297f4bc
HHT <harashtatandjung@gmail.com>
2020-03-03 18:48:39 +0700

    test1

--- test_ext.sql
+++ test_ext.sql
@@ -1,3 +1,6 @@
 -- Get a list of databases
 SELECT TOP 100* FROM sys.Sales
 GO;
+
+-- #https://docs.microsoft.com/en-us/sql/visual-studio-code/sql-server-develop-use-vscode?view=sql-server-ver15
+-- https://www.youtube.com/watch?v=7_fNMy6cKEk
