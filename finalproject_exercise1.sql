be3d17ebc1c38abc6482d2aca4d366b7e63e63e9
HHT <harashtatandjung@gmail.com>
2020-03-03 20:15:55 +0700

    Add Final Project Queries Questions

---
+++ finalproject_exercise1.sql
@@ -0,0 +1,34 @@
+SELECT * FROM finalproject.employeeattrition;
+
+SELECT AVG('Age') FROM finalproject.employeeattrition;
+
+SELECT COUNT('BusinessTravel') from employeeattrition where Attrition= 'No';
+
+SELECT JobRole, COUNT(*) as jumlah FROM employeeattrition where Attrition = 'No'
+GROUP BY JobRole;
+
+SELECT Attrition, AVG(MonthlyRate) from employeeattrition GROUP BY Attrition;
+
+SELECT Attrition, JobRole, MaritalStatus, MonthlyIncome from employeeattrition;
+
+SELECT JobRole, avg(MonthlyIncome) as ratarata from employeeattrition WHERE Attrition= 'Yes' GROUP BY JobRole
+HAVING ratarata>5000;
+
+-- SELECT * from employeeattrition WHERE Attrition='Yes' GROUP BY MaritalStatus HAVING NumCompaniesWorked >1;
+
+select AVG(DistanceFromHome) as jarak, JobRole from employeeattrition GROUP BY JobRole HAVING jarak > 5;
+
+-- SELECT COUNT(JobRole), avg(MonthlyIncome) as RATA 
+-- from employeeattrition 
+-- GROUP BY JobRole 
+-- HAVING RATA> RATA WHERE JobRole = 'Sales Executive';
+
+SELECT AVG(MonthlyIncome), JobRole
+from employeeattrition
+WHERE JobRole = 'Sales Executive';
+
+SELECT count(*)
+from employeeattrition
+WHERE JobRole = 'Manager' and MonthlyIncome > (SELECT AVG(MonthlyIncome)
+from employeeattrition
+WHERE JobRole = 'Sales Executive');
