0f44c7be909dd44605b69c3f2583ad7ae8bcae3e
HHT <harashtatandjung@gmail.com>
2020-03-03 18:04:35 +0700

    Edit notes

--- UdemySQL_s5.26.sql
+++ UdemySQL_s5.26.sql
@@ -1,2 +1,20 @@
+#session 5 lesson 26++
+#Datatypes: String
+
 show databases;
+
+use Sales;
+
+CREATE SCHEMA IF NOT EXISTS Sales;
+
+USE SALES;
+
+# 				(bytes)							Example
+#				max size	storage				len(symbol)	size(bytes)
+# 'CHAR(5)' 	255			Fixed		'James	5			5
+-- 										'Bob'	3			5
+# 'VARCHAR(5)'	65535		Variable	'James'	5			5
+-- 										'Bob'	3			3
+# 'ENUM('M', 'F')					
+
 
