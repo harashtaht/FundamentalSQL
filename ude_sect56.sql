048ae3538f4c2bb51df45e2f2502f784923897e1
HHT <harashtatandjung@gmail.com>
2020-03-04 20:23:56 +0700

    L54a

--- ude_sect56.sql
+++ ude_sect56.sql
@@ -173,3 +173,31 @@ dROP TABLE companies;
 
 
 -- L54
+
+CREATE TABLE customers
+(
+	customer_id INT AUTO_INCREMENT,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email_address VARCHAR(255),
+    number_of_complaints INT,
+PRIMARY KEY (customer_id)
+);
+
+ALTER TABLE customers
+ADD COLUMN gender ENUM('M', 'F') AFTER last_name;
+
+INSERT INTO customers (first_name, last_name, gender, email_address, number_of_complaints)
+VALUES ('John', 'Mackinley', 'M', 'john.mckinley', 0)
+;
+
+select * from customers;
+
+ALTER TABLE customers
+CHANGE COLUMN number_of_complaints number_of_complaints INT DEFAULT 0;
+
+INSERT INTO customers (first_name, last_name, gender)
+VALUES ('Peter', 'Figaro', 'M')
+;
+
+SELECT * from customers;
