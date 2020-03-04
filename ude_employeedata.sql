23b8c51fa12ace1ae94a3f00f885d34d798b3475
HHT <harashtatandjung@gmail.com>
2020-03-04 23:13:56 +0700

    L86 - LIKE NOT LIKE Exercise

--- ude_employeedata.sql
+++ ude_employeedata.sql
@@ -121,4 +121,16 @@ SELECT * FROM employees WHERE first_name NOT LIKE ('Mar%');
 
 -- SQL is not Case Sensitive
 
+-- Exercise 
 
+SELECT *
+FROM employees
+WHERE first_name LIKE ('Mark%');
+
+SELECT *
+FROM employees
+WHERE hire_date LIKE ('2000%');
+
+SELECT *
+FROM employees
+WHERE emp_no LIKE ('1____');
