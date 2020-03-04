7c09ef5dffe36e8326075c5b73bcbf57712d1583
HHT <harashtatandjung@gmail.com>
2020-03-04 19:30:08 +0700

    L49a

--- ude_sect56.sql
+++ ude_sect56.sql
@@ -121,5 +121,30 @@ CREATE TABLE customers
     PRIMARY KEY (customer_id)
 );
 
+-- DROP TABLE items;-- 
 
-    
+CREATE TABLE items
+(
+	item_code VARCHAR(255),
+    item VARCHAR(255),
+    unit_price NUMERIC(10,2),
+    company_id VARCHAR(255),
+PRIMARY KEY (item_code)
+);
+
+
+CREATE TABLE companies
+(
+	company_id VARCHAR(255),
+    company_name VARCHAR(255),
+    headquarters_phone_number INT(12),
+PRIMARY KEY (company_id)
+);    
+
+show tables;
+
+-- L49 FOREIGN KEY Constraints
+
+-- FOREIGN KEY: is mentioned in the 'child' table
+-- REFERENCES referencing the 'parent' table and the column referenced
+-- ON DELETE CASCADE: if a specific value from parent table is removed, in child table removed also
