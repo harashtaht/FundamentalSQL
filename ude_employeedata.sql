4ed410bdf95b3221a917eb49d85505b4fdc22b66
HHT <harashtatandjung@gmail.com>
2020-03-19 21:37:58 +0700

    L180

--- ude_employeedata.sql
+++ ude_employeedata.sql
@@ -893,6 +893,9 @@ VALUES ('110228', 'd003', '1992-03-21', '9999-01-01');
 INSERT INTO departments_dup
 VALUES ('d009', 'Customer Service');
 
+DELETE FROM departments_dup 
+WHERE dept_name = 'd009';
+
 ## Left Join
 
 SELECT 
@@ -904,7 +907,74 @@ FROM
 GROUP BY m.emp_no
 ORDER BY dept_no;
 
+SELECT 
+    m.dept_no, m.emp_no, d.dept_name
+FROM
+    dept_manager_dup m
+        JOIN
+    departments_dup d ON m.dept_no = d.dept_no
+GROUP BY m.emp_no
+ORDER BY dept_no;
+
 use udemy_employee;
+
+select * from departments_dup ORDER BY dept_no;
+
+# L177: LEFT JOIN part II
+
+SELECT 
+    d.dept_no, m.emp_no, d.dept_name
+FROM
+    departments_dup d
+        LEFT OUTER JOIN
+    dept_manager_dup m ON m.dept_no = d.dept_no
+ORDER BY d.dept_no;
+
+
+SELECT 
+    d.dept_no, m.emp_no, d.dept_name
+FROM
+    dept_manager_dup m
+        LEFT JOIN
+    departments_dup d ON m.dept_no = d.dept_no
+    WHERE dept_name IS NULL
+ORDER BY m.dept_no;
+
+SELECT * FROM employees;
+
+SELECT * FROM dept_manager_dup;
+
+SELECT 
+    e.emp_no, e.first_name, e.last_name, m.dept_no, m.from_date
+FROM
+    employees e
+       LEFT JOIN
+    dept_manager m ON e.emp_no = m.emp_no
+WHERE
+    e.last_name = 'Markovitch';
+
+SELECT 
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    dm.dept_no,
+    dm.from_date
+FROM
+    employees e
+        LEFT JOIN
+    dept_manager dm ON e.emp_no = dm.emp_no
+WHERE
+    e.last_name = 'Markovitch'
+ORDER BY dm.dept_no DESC , e.emp_no;
+
+select * from dept_manager;
+
+select * from employees;
+select * from dept_manager;
+
+## L180: RIGHT JOIN
+
+
 
 ####
 #### Section 15: Subqueries
