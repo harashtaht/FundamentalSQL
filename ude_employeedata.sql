4bb5618e930053061371fff8098df7a7b77abca5
HHT <harashtatandjung@gmail.com>
2020-03-20 21:06:05 +0700

    L195 INIT

--- ude_employeedata.sql
+++ ude_employeedata.sql
@@ -1129,7 +1129,59 @@ ORDER BY e.emp_no , d.dept_name;
 
 ### L193: Using Aggregate Functions with Joins
 
+SELECT 
+    e.gender, AVG(s.salary) AS average_salary
+FROM
+    employees e
+        JOIN
+    salaries s ON e.emp_no = s.emp_no
+GROUP BY gender;
 
+### L194: JOIN more than two tables
+
+SELECT 
+    -- e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.hire_date,
+    dm.from_date,
+    d.dept_name
+FROM
+    employees e
+        CROSS JOIN
+    dept_manager dm
+        JOIN
+    departments d ON dm.dept_no = d.dept_no;
+-- ORDER BY e.emp_no;
+
+
+SELECT 
+    e.first_name,
+    e.last_name,
+    e.hire_date,
+    m.from_date,
+    d.dept_name
+FROM
+    employees e
+        JOIN
+    dept_manager m ON e.emp_no = m.emp_no
+        JOIN
+    departments d ON m.dept_no = d.dept_no
+;
+
+SELECT 
+    e.first_name,
+    e.last_name,
+    e.hire_date,
+    m.from_date,
+    d.dept_name
+FROM
+    departments d
+        JOIN
+    dept_manager m ON d.dept_no = m.dept_no
+        JOIN
+    employees e ON m.emp_no = e.emp_no
+;
 
 
 ####
