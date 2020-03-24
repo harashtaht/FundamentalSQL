4b92478d1478a88e7128e800170061e69e3d9904
HHT <harashtatandjung@gmail.com>
2020-03-24 17:43:23 +0700

    L212

--- ude_employeedata.sql
+++ ude_employeedata.sql
@@ -1369,3 +1369,192 @@ FROM dept_manager dm)
 ORDER BY e.emp_no;
 
 # Ex 204
+
+SELECT 
+    *
+FROM
+    dept_manager
+WHERE
+    emp_no IN (SELECT 
+            emp_no
+        FROM
+            employees
+        WHERE
+            hire_date BETWEEN '1990-01-01' AND '1995-01-01');
+            
+## L206: EXISTS-NOT EXISTS
+-- returns a Boolean value
+
+SELECT 
+    e.first_name, e.last_name
+FROM
+    employees e
+WHERE #the following condition is true
+    EXISTS( SELECT 
+            *
+        FROM
+            dept_manager dm
+        WHERE
+            dm.emp_no = e.emp_no);
+            
+# Ex
+
+select * from titles;
+
+SELECT * FROM employees e
+WHERE
+	EXISTS(SELECT *
+    FROM titles t
+    WHERE 
+    t.emp_no = e.emp_no
+    AND
+    t.title = 'Assistant Engineer');
+    
+SELECT 
+    *
+FROM
+    employees e
+        JOIN
+    titles t ON e.emp_no = t.emp_no
+    WHERE t.title = 'Assistant Engineer';
+    
+## L209: SQL Subqueries nested in SELECT and FROM
+
+SELECT * FROM dept_manager;
+
+SELECT * FROM departments;
+
+SELECT e.emp_no, e.first_name, de.dept_no FROM employees e
+JOIN dept_emp de
+ON e.emp_no = de.emp_no
+ORDER BY e.emp_no;
+
+SELECT * FROM dept_emp;
+
+# Exercise in slides
+
+SELECT 
+    A.*
+FROM
+    (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110022) AS manager_ID
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no <= 10020
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no) AS A
+UNION SELECT 
+    B.*
+FROM
+    (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110039) AS manager_ID
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no >= 10021 AND e.emp_no <= 10040
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no) AS B;
+
+DROP TABLE IF EXISTS emp_manager;
+
+CREATE TABLE emp_manager (
+	emp_no INT(11) NOT NULL,
+    dept_no CHAR(4) NULL,
+    manager_no INT(11) NOT NULL
+    );
+    
+
+
+SELECT 
+    A.*
+FROM
+    (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110022) AS manager_ID
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no <= 10020
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no) AS A
+UNION SELECT 
+    B.*
+FROM
+    (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110039) AS manager_ID
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no >= 10021 AND e.emp_no <= 10040
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no) AS B
+	UNION
+	SELECT 
+	C.* 
+    FROM (SELECT
+			e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110039) AS manager_ID
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no = 110022
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no) AS C
+    UNION
+    SELECT
+    D.* 
+    FROM (SELECT
+			e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110022) AS manager_ID
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no = 110039
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no) AS D;
