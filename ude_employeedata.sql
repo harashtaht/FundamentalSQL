b34d46de16a974b60cbef844b344527e86eac13d
HHT <harashtatandjung@gmail.com>
2020-03-24 14:37:04 +0700

    L203 INIT

--- ude_employeedata.sql
+++ ude_employeedata.sql
@@ -580,7 +580,16 @@ WHERE
 
 # L136: UPDATE part 2
 
+SELECT * FROM departments_dup ORDER BY dept_no;
 
+COMMIT;
+
+UPDATE departments_dup 
+SET 
+    dept_no = 'd011',
+    dept_name = 'Quality Control';
+    
+ROLLBACK;
 
 
 ### Section 12 : The DELETE Statement
