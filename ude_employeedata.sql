54b79534659eca5163218767db227631c44bd652
HHT <harashtatandjung@gmail.com>
2020-04-14 17:18:32 +0700

    Stored Proc Output -- How to call?

--- ude_employeedata.sql
+++ ude_employeedata.sql
@@ -1760,3 +1760,51 @@ CALL emp_avg_salary(10001);
 
 ## L226: Stored Procedure with an Output Parameter
 
+DELIMITER $$
+CREATE PROCEDURE emp_avg_salary_out(in p_emp_no INTEGER, out p_avg_salary DECIMAL(10,2))
+BEGIN
+SELECT 
+    AVG(s.salary)
+INTO p_avg_salary FROM
+    employees e
+        JOIN
+    salaries s ON e.emp_no = s.emp_no
+WHERE
+    e.emp_no = p_emp_no;
+END$$
+
+DELIMITER ;
+
+-- CALL udemy_employee.emp_avg_salary_out(10001, @p_avg_salary);
+-- Must be called from Thunder Symbol on the left?
+
+-- QUIZ
+DROP PROCEDURE IF EXISTS emp_info;
+
+DELIMITER $$
+CREATE PROCEDURE emp_info(in p_first_name VARCHAR(255), in p_last_name VARCHAR(255),
+out p_emp_no INTEGER)
+BEGIN
+SELECT 
+    e.emp_no
+INTO p_emp_no FROM
+    employees e
+WHERE
+    e.first_name = p_first_name
+        AND e.last_name = p_last_name;
+END$$
+DELIMITER ;
+
+CALL emp_info('Georgi', 'Facello', @p_emp_no); -- Error because there are two emp_no with the stated name
+
+
+SELECT emp_no
+FROM employees
+WHERE first_name = 'Georgi'
+AND last_name = 'Facello';
+
+CALL udemy_employee.emp_info('Bezalel', 'Simmel', @p_emp_no);
+
+-- Must read more here:
+-- https://code.tutsplus.com/articles/an-introduction-to-stored-procedures-in-mysql-5--net-17843
+
