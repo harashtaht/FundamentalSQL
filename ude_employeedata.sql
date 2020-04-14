bdf1feed6ad1cb5c6e0703771fb34d0563e4007b
HHT <harashtatandjung@gmail.com>
2020-04-14 18:16:33 +0700

    L235 - Functions

--- ude_employeedata.sql
+++ ude_employeedata.sql
@@ -1828,4 +1828,92 @@ CALL udemy_employee.emp_info('Kyoichi', 'Maliniak', @v_emp_no);
 SELECT @v_emp_no;
 
 ## L232: User-defined functions in MYSQL
+-- we don't indicate the object name, but data type
+-- output is outside the ()
+-- not 'SET', but 'DECLARE'
 
+DELIMITER $$
+CREATE FUNCTION f_emp_avg_salary (p_emp_no INTEGER) RETURNS DECIMAL(10,2)
+deterministic NO SQL READS SQL DATA
+BEGIN
+
+DECLARE v_avg_salary DECIMAL(10,2);
+
+SELECT 
+	AVG(s.salary)
+INTO v_avg_salary FROM
+	employees e
+    JOIN
+    salaries s ON e.emp_no = s.emp_no
+WHERE
+	e.emp_no = p_emp_no;
+    
+RETURN v_avg_salary;
+END$$
+
+DELIMITER ;
+    
+SELECT f_emp_avg_salary(11300);
+
+## L233: Error Code: 1418
+
+-- SET @@global.log_bin_trust_function_creators := 1;
+
+
+SELECT 
+    s.salary, s.from_date
+FROM
+    employees e
+        JOIN
+    salaries s ON e.emp_no = s.emp_no
+WHERE
+    e.first_name = 'Bezalel'
+        AND e.last_name = 'Simmel';
+
+-- Exercise
+DELIMITER $$
+CREATE FUNCTION f_emp_info (p_first_name VARCHAR(255), p_last_name VARCHAR(255))
+RETURNS DECIMAL(10,2)
+DETERMINISTIC NO SQL READS SQL DATA
+BEGIN
+
+DECLARE v_max_from_date DATE;
+DECLARE v_salary DECIMAL(10,2);
+
+-- get the latest from_date for the employee
+SELECT 
+    MAX(from_date)
+INTO v_max_from_date FROM
+    employees e
+        JOIN
+    salaries s ON e.emp_no = s.emp_no
+WHERE
+    e.first_name = p_first_name
+        AND e.last_name = p_last_name;
+
+-- get the salary by using v_max_from_date variable
+SELECT 
+    s.salary
+INTO v_salary FROM
+    employees e
+        JOIN
+    salaries s ON e.emp_no = s.emp_no
+WHERE
+    e.first_name = p_first_name
+        AND e.last_name = p_last_name
+        AND s.from_date = v_max_from_date;
+        
+RETURN v_salary;
+
+END$$
+
+DELIMITER ;
+
+SELECT f_emp_info('Georgi', 'Facello');
+
+SELECT *
+FROM salaries
+WHERE emp_no = 10001
+ORDER BY from_date DESC;
+
+## L236: Conclusion
