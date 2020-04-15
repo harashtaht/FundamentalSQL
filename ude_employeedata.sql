2d3a826204c87b3106d811aa64591baf8d43653c
HHT <harashtatandjung@gmail.com>
2020-04-15 18:58:24 +0700

    L251

--- ude_employeedata.sql
+++ ude_employeedata.sql
@@ -2067,3 +2067,39 @@ DROP INDEX i_hire_date;
 SELECT * FROM salaries WHERE salary>89000;
 
 CREATE INDEX i_salary ON salaries(salary);
+
+## L249: The Case Statement
+
+SELECT 
+    emp_no,
+    first_name,
+    last_name,
+    CASE gender
+        WHEN 'M' THEN 'Male'
+        ELSE 'Female'
+    END AS Gender
+    FROM employees;
+ 
+SELECT 
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    CASE
+        WHEN dm.emp_no IS NOT NULL THEN 'Manager'
+        ELSE 'Employee'
+    END AS is_manager
+FROM
+    employees e
+        LEFT JOIN
+    dept_manager dm ON dm.emp_no = e.emp_no
+WHERE
+    e.emp_no > 109990;
+    
+    
+SELECT 
+    emp_no,
+    first_name,
+    last_name,
+    IF(gender = 'M', 'Male', 'Female') AS gender
+FROM
+    employees;
