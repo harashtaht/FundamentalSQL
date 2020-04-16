ef231f99d043e49a64f31aa0835757cbdd37aba5
HHT <harashtatandjung@gmail.com>
2020-04-16 16:41:01 +0700

    Task 2

--- ude_employeedata.sql
+++ ude_employeedata.sql
@@ -2240,3 +2240,12 @@ GROUP BY calendar_year, e.gender
 HAVING calendar_year>=1990
 ORDER BY calendar_year;
 
+## L270: Task 2 
+
+/* Compare the number of male managers
+to the number of female managers
+from different departments for each year,
+starting from 1990 */
+
+
+
