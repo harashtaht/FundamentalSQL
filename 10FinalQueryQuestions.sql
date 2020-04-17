83b4d9dbc03ca2af4aaf73a8f52a73ad39656508
HHT <harashtatandjung@gmail.com>
2020-04-17 11:10:31 +0700

    Exercise 3 INIT

--- 10FinalQueryQuestions.sql
+++ 10FinalQueryQuestions.sql
@@ -1,4 +1,5 @@
--- Exercise 1
+## Exercise 1 ##
+
 /* Find the Average Salary of male and female 
 employees in each department
 */
@@ -13,14 +14,24 @@ select * from employees;
 select * from dept_emp;
 
 select 
-	e.emp_no,
-    e.gender,
     de.dept_no,
+    e.gender,
     AVG(s.salary)
 FROM
 	employees e
     JOIN salaries s ON e.emp_no = s.emp_no
     JOIN dept_emp de ON e.emp_no = de.emp_no
-GROUP BY de.dept_no;
+    GROUP BY de.dept_no, e.gender
+    ORDER BY dept_no ASC;
 	
-	
+## Exercise 2
+/* Find the lowest department number
+encountered in the dept_emp table. Then, find the highest department number. */
+
+SELECT 
+    MIN(dept_no), MAX(dept_no)
+FROM
+    dept_emp;
+    
+## Exercise 3
+/* 
