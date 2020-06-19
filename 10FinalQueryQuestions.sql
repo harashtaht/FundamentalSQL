754add34236afda3611e1222fc369c796af61af6
HHT <harashtatandjung@gmail.com>
2020-06-20 06:10:27 +0700

    SQL INIT

--- 10FinalQueryQuestions.sql
+++ 10FinalQueryQuestions.sql
@@ -34,4 +34,12 @@ FROM
     dept_emp;
     
 ## Exercise 3
-/* 
+
+SELECT 
+    e.emp_no, de.dept_no
+FROM
+    employees e
+        JOIN
+    dept_emp de ON e.emp_no = de.emp_no
+HAVING e.emp_no <= 10040
+ORDER BY e.emp_no ASC;
